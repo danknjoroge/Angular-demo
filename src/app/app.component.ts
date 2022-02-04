7c1b2be0b775390fb21d8d8f16b8296862683fe8
danknjoroge <dank8505@gmail.com>
2022-02-04 08:04:08 +0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'This is my First Angular application!! ';
+
+  name = "Javascript";
+
+  description= "Hello there!";
+  // disabledBox=true;
+  // enableBox{
+  //   this.disabledBox=false;
+  // }
+}
